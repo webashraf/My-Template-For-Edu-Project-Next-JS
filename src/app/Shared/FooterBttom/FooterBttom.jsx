010eba63ee69351f0084baa7f42946e64778e2a6
webashraf <aliashraftamim@gmail.com>
2023-09-01 21:34:47 +0600

    add my custom template

---
+++ src/app/Shared/FooterBttom/FooterBttom.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FooterBttom = () => {
+    return (
+        <div>
+            <div>Footer</div>
+        </div>
+    );
+};
+
+export default FooterBttom;
---
+++ src/app/Shared/NavBar/NavBar.jsx
@@ -0,0 +1,69 @@
+'use client';
+
+import { Dropdown, Navbar, Avatar, Item } from 'flowbite-react';
+import './NavBar.css'
+const NavBar = () => {
+    return (
+        <Navbar
+            fluid
+            rounded
+        >
+            <Navbar.Brand href="https://flowbite-react.com">
+                <h2 className='text-2xl font-serif uppercase text-blue-900'>Bornomala-edu</h2>
+            </Navbar.Brand>
+            <div className="flex md:order-2">
+                <Dropdown
+                    inline
+                    label={<Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded />}
+                >
+                    <Dropdown.Header>
+                        <span className="block text-sm">
+                            Bonnie Green
+                        </span>
+                        <span className="block truncate text-sm font-medium">
+                            name@flowbite.com
+                        </span>
+                    </Dropdown.Header>
+                    <Item>
+                        Dashboard
+                    </Item>
+                    <Item>
+                        Settings
+                    </Item>
+                    <Item>
+                        Earnings
+                    </Item>
+                    <Dropdown.Divider />
+                    <Item>
+                        Sign out
+                    </Item>
+                </Dropdown>
+                <Navbar.Toggle />
+            </div>
+            <Navbar.Collapse>
+                <Navbar.Link
+                    active
+                    href="#"
+                >
+                    <p>
+                        Home
+                    </p>
+                </Navbar.Link>
+                <Navbar.Link href="#">
+                    About
+                </Navbar.Link>
+                <Navbar.Link href="#">
+                    Services
+                </Navbar.Link>
+                <Navbar.Link href="#">
+                    Pricing
+                </Navbar.Link>
+                <Navbar.Link href="#">
+                    Contact
+                </Navbar.Link>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+}
+
+export default NavBar;
